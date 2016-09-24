b47bc3799575e7b8fc62e24197dc128cbc8baebb
Patel Drashti <19drashti96@gmail.com>
2016-09-24 13:43:34 +0800

    Invert dependency from Logic to Storage

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,18 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+
+public abstract class Storage {
+
+    public abstract AddressBook load() throws StorageOperationException;
+
+    public abstract String getPath();
+
+    public abstract void save(AddressBook addressBook) throws StorageOperationException;
+    
+    public static class StorageOperationException extends Exception {
+        public StorageOperationException(String message) {
+            super(message);
+        }
+    }
+}
